c1e746136985b1a9dc7ad8c288a975ff6d1d5cc9
patricialan <54603331+patricialan@users.noreply.github.com>
2021-05-31 15:58:49 -0600

    fixed escape problem

--- models/wait.js
+++ models/wait.js
@@ -9,7 +9,7 @@ var WaitSchema = new Schema(
         party_num: {type: Number, min: 1, required: [true, 'Number of people required']},
         seating: {
             type: String,
-            default: 'Patio (No Preference for Front/Back)'
+            default: 'Patio (No Preference for Front or Back)'
         },
         has_cell: {
             type: String
